fdc64cfed74f9e7b0c11e61aeb28c232aa9ae111
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-11-06 15:30:56 +0900

    feat: install smui + ヘッダー作成

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,49 @@
 <script async script lang="ts">
-    
+    import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+    import Button, { Label } from '@smui/button';
 </script>
+
+<svelte:head>
+    <!-- Material Icons -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+    <!-- Roboto -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+    />
+    <!-- Roboto Mono -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+    />
+    <link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+</svelte:head>
+
+<TopAppBar variant="static">
+    <Row>
+        <Section>
+            <Title>Svelte Form Sample</Title>
+        </Section>
+        <Section align="end">
+            <Button
+                href="https://github.com/eteeeeeerminal/svelte-form-sample"
+                variant="unelevated"
+            >
+                <Label>GitHub</Label>
+            </Button>
+        </Section>
+    </Row>
+</TopAppBar>
 
 <main>
     <slot />
 </main>
+
+<style>
+    :global(body) {
+        margin: 0;
+    }
+</style>
