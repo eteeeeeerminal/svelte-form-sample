8284550c113c2201aee30ca250b0dab4bf0bbda2
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-11-06 16:38:20 +0900

    feat: smui only の最低限のサンプル

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<h1>Svelte でフォームを作るサンプルです。</h1>
+<ul>
+    <li>
+        <a href={`${base}/smui-only`}>Svelte Material UI Only</a>
+    </li>
+</ul>
---
+++ src/routes/smui-only/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    import Button, { Label } from '@smui/button';
+	import FormField from '@smui/form-field';
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
+	import Select, { Option } from '@smui/select';
+    import Radio from '@smui/radio';
+    import CheckBox from '@smui/checkbox';
+
+    let textValue = "";
+    let selectSelected = "";
+    const selectOptions = [
+        "ほげ", "ふが", "ぴよ"
+    ];
+    let radioSelected = "";
+    const radioOptions = [
+        "Vue", "React", "Svelte"
+    ];
+    let checkboxChecked: string[] = [];
+    const checkboxOptions = [
+        "fizz", "buzz"
+    ];
+
+    let submitted: object | null = null;
+    const onSubmit = () => {
+        submitted = {
+            textValue,
+            selectSelected,
+            radioSelected,
+            checkboxChecked
+        }
+    };
+</script>
+
+<h2>Svelte Material UI のサンプル</h2>
+<p>Svelte Material UI だけで作るとこんな感じ。</p>
+
+<form>
+    <Textfield bind:value={textValue} label="プレースホルダー">
+        <HelperText slot="helper">細かい説明</HelperText>
+    </Textfield>
+    <p>入力しているテキスト: {textValue}</p>
+
+    <Select bind:value={selectSelected} label="選んでね" variant="standard">
+        <Option value="" />
+        {#each selectOptions as value}
+            <Option {value}>{value}</Option>
+        {/each}
+    </Select>
+    <p>選択された要素: {selectSelected}</p>
+
+    {#each radioOptions as value}
+        <FormField>
+            <Radio bind:group={radioSelected} {value} />
+            <span slot="label">{value}</span>
+        </FormField>
+    {/each}
+    <p>選択された要素: {radioSelected}</p>
+
+    {#each checkboxOptions as value}
+        <FormField>
+            <CheckBox bind:group={checkboxChecked} {value} />
+            <span slot="label">{value}</span>
+        </FormField>
+    {/each}
+    <p>選択された要素: {checkboxChecked}</p>
+
+    <Button color="primary" variant="raised" type="button" on:click={onSubmit}>
+        <Label>送信</Label>
+    </Button>
+</form>
+
+{#if submitted != null}
+    <h3>入力された結果</h3>
+    <pre>{JSON.stringify(submitted, null, 2)}</pre>
+{/if}
